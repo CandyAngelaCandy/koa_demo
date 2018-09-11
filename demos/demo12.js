5c737ccb8ed5591cd1e05b1997749dbf10609cd7
yyhuang <yyhuang@thoughworks.com>
2018-09-11 15:53:34 +0800

    koa-compose

---
+++ demos/demo12.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Koa = require('koa');
+const app = new Koa();
+const compose = require('koa-compose');
+
+const logger = (ctx, next) => {
+    console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`);
+    next();
+};
+
+const main = ctx => {
+    ctx.response.body = 'Hello World';
+};
+const middlewares = compose([logger,main]);
+app.use(middlewares);
+
+app.listen(3000);
+
+console.log('Running a koa server at localhost:3000')
