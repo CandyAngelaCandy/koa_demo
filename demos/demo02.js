500612820d631cce91f0f2e8dc9c4207d962167b
yyhuang <yyhuang@thoughworks.com>
2018-09-11 23:01:44 +0800

    koa-body extract data from post request

--- demos/demo02.js
+++ demos/demo02.js
@@ -11,4 +11,4 @@ app.use(main);
 
 app.listen(3000);
 
-console.log('Running a koa server at localhost:3000')
+console.log('Running a koa server at localhost:3000');
---
+++ demos/demo18.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Koa = require('koa');
+const app = new Koa();
+
+const main = ctx => {
+    const n = Number(ctx.cookies.get("view")|| 0)+1;
+    ctx.cookies.set("view",n);
+    ctx.response.body = n + 'views';
+};
+
+app.use(main);
+
+app.listen(3000);
+
+console.log('Running a koa server at localhost:3000');
