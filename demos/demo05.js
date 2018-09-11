da69ec1846f4d37bc9d40128fd503a42119f67eb
yyhuang <yyhuang@thoughworks.com>
2018-09-11 13:49:32 +0800

    native route

---
+++ demos/demo05.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Koa = require('koa');
+const app = new Koa();
+
+const main = ctx => {
+    if (ctx.request.path !== '/') {
+        ctx.response.type = 'html';
+        ctx.response.body = '<a href="/">Index Page</a>';
+    } else {
+        ctx.response.body = 'Hello World';
+    }
+};
+
+app.use(main);
+
+app.listen(3000);
+
+console.log('Running a koa server at localhost:3000')
