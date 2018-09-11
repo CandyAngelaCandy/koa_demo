7aff1292068d3b96d93f76021157cf3df22a0d22
yyhuang <yyhuang@thoughworks.com>
2018-09-11 13:12:47 +0800

    add context obj

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 const Koa = require('koa');
 const app = new Koa();
 
+const main = ctx => {
+    ctx.response.body = 'Hello World';
+};
+
+app.use(main);
+
 app.listen(3000);
 
 console.log('Running a koa server at localhost:3000')
