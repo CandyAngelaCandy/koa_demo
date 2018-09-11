8e850341a2b03c92ca1cdd2532da508800aef8b9
yyhuang <yyhuang@thoughworks.com>
2018-09-11 16:04:55 +0800

    throw 500 404 error

---
+++ demos/demo14.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Koa = require('koa');
+const app = new Koa();
+
+const main = ctx => {
+    ctx.response.status = 404;
+    ctx.response.body = 'Page Not Found';
+};
+
+app.use(main);
+
+app.listen(3000);
+
+console.log('Running a koa server at localhost:3000')
